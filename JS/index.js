1bb7d1306d3ac138651f2e86489b1ecc5400c007
ivan-vz <ivanveci28@gmail.com>
2023-05-24 23:49:09 -0300

    Addition of % and AC, change of div tag for button tag and i tag for just text

--- JS/index.js
+++ JS/index.js
@@ -1,9 +1,10 @@
 let keys = document.querySelectorAll(".key");
 let screen = document.querySelector(".window");
-let operators = ["*", "-", "+", "/"]
+let operators = ["*", "-", "+", "/", "%"];
 let equation = "";
 let equalPressed = false;
 
+//Function to write the caracter on the screen and add it to the ecuation
 keys.forEach((key) => {
     key.addEventListener("click", () => {
         if(equalPressed){
@@ -15,22 +16,27 @@ keys.forEach((key) => {
     })
 })
 
+//Function to indicate where the equation end and start solving it
 let equal = document.querySelector("#equal");
 equal.addEventListener("click", () => {
+    if(equation === ""){ return }
+
     equation = equation.split(" ");
     if(!validEquation(equation)){ 
         screen.textContent = 'ERROR';
+        equalPressed = true;
         return;
     }
 
-    screen.textContent = agrupar(equation);
+    screen.textContent = solve(equation);
     equalPressed = true;
 })
 
+//Function to check if the ecuation given is correct
 let validEquation = (array) => {
     let i = 0;
     while (i < array.length) {
-      if ((i % 2 !== 0 && !isNaN(parseInt(array[i]))) || (i % 2 === 0 && isNaN(parseInt(array[i])))) {
+      if ((i % 2 !== 0 && !isNaN(parseFloat(array[i]))) || (i % 2 === 0 && isNaN(parseFloat(array[i])))) {
         return false;
       }
       i++;
@@ -38,14 +44,17 @@ let validEquation = (array) => {
     return true;
 }
 
-let agrupar = (arr) => {
+//Function to resolve the equation
+let solve = (arr) => {
     let i = 0;
-    while (arr.includes("*") || arr.includes("/")) {
+    while (arr.includes("*") || arr.includes("/") || arr.includes("%")) {
       if (arr[i + 1] === "*") {
-        arr.splice(i, 3, parseInt(arr[i]) * parseInt(arr[i + 2]));
+        arr.splice(i, 3, parseFloat(arr[i]) * parseFloat(arr[i + 2]));
       } else if (arr[i + 1] === "/") {
-        arr.splice(i, 3, parseInt(arr[i]) / parseInt(arr[i + 2]));
-      } else {
+        arr.splice(i, 3, parseFloat(arr[i]) / parseFloat(arr[i + 2]));
+      } else if(arr[i + 1] === "%") {
+        arr.splice(i, 3, parseFloat(arr[i]) % parseFloat(arr[i + 2]));
+      }else {
         i++;
         continue;
       }
@@ -54,9 +63,9 @@ let agrupar = (arr) => {
     let j = 0;
     while (arr.includes("+") || arr.includes("-")) {
         if (arr[j + 1] === "+") {
-          arr.splice(j, 3, parseInt(arr[j]) + parseInt(arr[j + 2]));
+          arr.splice(j, 3, parseFloat(arr[j]) + parseFloat(arr[j + 2]));
         } else if (arr[j + 1] === "-") {
-          arr.splice(j, 3, parseInt(arr[j]) - parseInt(arr[j + 2]));
+          arr.splice(j, 3, parseFloat(arr[j]) - parseFloat(arr[j + 2]));
         } else {
           j++;
           continue;
@@ -65,3 +74,18 @@ let agrupar = (arr) => {
 
     return arr[0];
   }
+
+//Function to clean eraser all the equation
+let clean = document.querySelector(".clear");
+clean.addEventListener("click", () => {
+  equation = "";
+  screen.textContent = equation;
+})
+
+//Function to move between caracters
+let moves = document.querySelectorAll(".move");
+moves.forEach((move) => {
+  move.addEventListener("click", () => {
+
+  })
+})
