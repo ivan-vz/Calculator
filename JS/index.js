9bc3320cfdfff9612f4d1144df55427c12b5ef79
ivan-vz <ivanveci28@gmail.com>
2023-05-26 01:59:50 -0300

    JavaScript update:     -Addition of the power function (^).     -Modification of equation handling (currently with arrays and functions that "translate" the equation to a more readable one)     Updated the "display" from text-only to a flex with different divs representing each of the values given by the user.

--- JS/index.js
+++ JS/index.js
@@ -1,38 +1,45 @@
 let keys = document.querySelectorAll(".key");
-let screen = document.querySelector(".window");
-let operators = ["*", "-", "+", "/", "%"];
-let equation = "";
+let screen = document.querySelector(".screen");
+let operators = ["*", "-", "+", "/", "%", "^"];
+let equation = [];
 let equalPressed = false;
+let pointer = 0;
 
-//Function to write the caracter on the screen and add it to the ecuation
+//Function to write the character on the screen and add it to the equation
 keys.forEach((key) => {
     key.addEventListener("click", () => {
         if(equalPressed){
             equation = "";
             equalPressed = false; 
         }
-        equation += `${key.dataset.value}`;
-        screen.textContent = equation;
+        
+        equation.push(key.dataset.value);
+        let div = document.createElement("div");
+        div.setAttribute("data-info", `{index: ${equation.length - 1}, value: ${key.dataset.value}}`)
+        div.textContent = key.dataset.value;
+        screen.appendChild(div);
+        pointer = equation.length - 1;
     })
 })
 
 //Function to indicate where the equation end and start solving it
 let equal = document.querySelector("#equal");
 equal.addEventListener("click", () => {
-    if(equation === ""){ return }
+    if(equation === []){ return }
 
-    equation = equation.split(" ");
-    if(!validEquation(equation)){ 
+    let readableEquation = makeReadable(equation);
+
+    if(!validEquation(readableEquation)){ 
         screen.textContent = 'ERROR';
         equalPressed = true;
         return;
     }
 
-    screen.textContent = solve(equation);
+    screen.textContent = solve(readableEquation);
     equalPressed = true;
 })
 
-//Function to check if the ecuation given is correct
+//Function to check if the equation given is correct
 let validEquation = (array) => {
     let i = 0;
     while (i < array.length) {
@@ -44,8 +51,65 @@ let validEquation = (array) => {
     return true;
 }
 
+//Function to make readable a equation
+let makeReadable = (eq) => {
+  let arr =  eq.map((val) => val.trim());
+  
+  let signs = ['+', '-'];
+  let op = ["*", "/", "%", "^"];
+
+
+  let i = 0;
+  while(arr[i+1] !== undefined){
+    if(signs.includes(arr[i]) && signs.includes(arr[i+1])){
+      if(arr[i] === "+" && arr[i+1]=== "+"){
+        arr.splice(i, 2, "+");
+      }
+
+      if(arr[i] === "-" && arr[i+1]=== "-"){
+        arr.splice(i, 2, "+");
+      }
+
+      if((arr[i] === "-" && arr[i+1]=== "+") || (arr[i] === "+" && arr[i+1]=== "-")){
+        arr.splice(i, 2, "-");
+      }
+
+      i--;
+    } else {
+      i++;
+    }
+  }
+
+  let k = 0;
+  while(arr[k+1] !== undefined){
+    if (k > 20){ return }
+    if(op.includes(arr[k]) && signs.includes(arr[k+1])){
+      arr.splice(k+1, 2, `${arr[k+1]}${arr[k+2]}`);
+      k--;
+    } else {
+      k++;
+    }
+  }
+
+  return arr;
+}
+
 //Function to resolve the equation
 let solve = (arr) => {
+    let k = 0;
+    while (arr.includes("^")) {
+      if (arr[k + 1] === "^") {
+        let pot = 1;
+        for(let t = 0; t < parseInt(arr[k+2]); t++){
+          pot *= parseFloat(arr[k]);
+        }
+        arr.splice(k, 3, pot);
+      } else {
+        k++;
+        continue;
+      }
+    }
+
     let i = 0;
     while (arr.includes("*") || arr.includes("/") || arr.includes("%")) {
       if (arr[i + 1] === "*") {
@@ -82,10 +146,14 @@ clean.addEventListener("click", () => {
   screen.textContent = equation;
 })
 
-//Function to move between caracters
+//Function to move between characters
 let moves = document.querySelectorAll(".move");
 moves.forEach((move) => {
   move.addEventListener("click", () => {
-
+    if(equation !== []){ 
+      /*
+      ya fueron creados los div con un data-info con los valores del interior y el del indice, ahora toca como ver para mover el focus y poder cambiar el valor si agregar otro elemento
+      */
+    }
   })
 })
